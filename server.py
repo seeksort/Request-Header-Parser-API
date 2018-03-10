39c3fc5dbd375bbdf5d48b8ef6201976c9182493
Kristin Faner <kfaner@gmail.com>
2018-03-10 12:17:13 -0600

    add whoami route; remove node files

--- server.py
+++ server.py
@@ -4,3 +4,12 @@ app = Flask(__name__)
 @app.route('/')
 def root():
     return render_template('index.html')
+
+@app.route('/api/whoami')
+def whoami():
+    response_dict = {
+        "ipaddress": request.remote_addr,
+        "language": request.headers["Accept-Language"].split(';')[0].split(',')[0],
+        "software": request.headers["User-Agent"].split('(')[1].split(')')[0],
+    }
+    return jsonify(response_dict)
